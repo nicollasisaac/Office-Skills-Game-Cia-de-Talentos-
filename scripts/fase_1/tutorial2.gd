40b08c3add813dba067e327ab90c20d635794ce0
GuilhermeLinhares04 <123950223+GuilhermeLinhares04@users.noreply.github.com>
2023-03-07 21:22:43 -0300

    att bug fase 1

--- scripts/fase_1/tutorial2.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-# Referência ao AnimationPlayer / AnimatioPlayer reference
-var animation_player = null
-onready var positionRafa = get_node("Personagens/fase1_rafa_protagonista")
-
-# quando rafa chega na cafeteira o texto some / when rafa reaches the coffe machine the text disappears
-func _process(delta): 
-	pass
